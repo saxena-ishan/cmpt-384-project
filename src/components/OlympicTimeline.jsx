959c39dbdc1441aa0a2ebb3d0d611f159ad98c4a
Ricardo Rheeder <ricardorheeder@gmail.com>
2019-11-27 22:49:04 -0600

    Edited marker events

--- src/components/OlympicTimeline.jsx
+++ src/components/OlympicTimeline.jsx
@@ -8,25 +8,44 @@ import {
     YouTubeEvent,
 } from '@merc/react-timeline';
 
+
 export default class OlympicTimeline extends Component {
+
     constructor(props) {
         super(props);
+        this.state = {
+            isOpen: true
+        }
     }
 
+    MyCustomMarker = () => <span onClick={(() => this.testButton())}>😀</span>;
+
+    testButton() {
+        this.setState({ isOpen: !this.state.isOpen })
+    }
 
 
     render() {
         const { year } = this.props;
         return (
-            <Timeline>
-                <Events>
-                    <TextEvent date={year} text="**Markdown** is *supported*">
-                        <div>
-                            Hi
-                        </div>
-                    </TextEvent>
-                </Events>
-            </Timeline>
+            <div>
+
+                <Timeline>
+                    <Events>
+
+                        {
+                            this.state.isOpen ?
+                                <TextEvent date={year} text="**Markdown** is *supported*" marker={() => this.MyCustomMarker()}></TextEvent>
+                                :
+                                <TextEvent date={year} text="" marker={() => this.MyCustomMarker()}></TextEvent>
+
+                        }
+
+                    </Events>
+                </Timeline> : <div></div>
+
+            </div>
+
         );
     }
 }
