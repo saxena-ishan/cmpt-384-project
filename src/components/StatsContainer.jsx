690d58f835734f2c3de1f84076d5b8f01b37e262
saxena-ishan <saxena.ishan2204@gmail.com>
2019-12-01 15:22:36 -0600

    added placeholder for TopGames componenet

--- src/components/StatsContainer.jsx
+++ src/components/StatsContainer.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { RingChart } from "./RingChart"
+import { TopGames } from "./TopGames"
+
 export default class StatsContainer extends Component {
     render() {
         return (
             <div className="stats-container-root">
                 <RingChart />
+                <TopGames />
             </div>
         )
     }
