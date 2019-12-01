cc108e8ac57cef4206ab4dcb535937f64a1f5c4c
saxena-ishan <saxena.ishan2204@gmail.com>
2019-12-01 08:18:09 -0600

    connected the redux store to the main app

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Home } from './pages';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/configureStore';
 
 
 //Root sass file for webpack to compile
 import './sass/main';
 
 //Initial Default Redux Settings  
-// const store = configureStore();
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Provider store={store}>
+        <Home/>
+      </Provider>
 
-      // <div className="boilerplate-div">react-boilerplate</div>
     )
   }
 }
