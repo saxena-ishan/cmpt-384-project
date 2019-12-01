f34a0cd66448da2b5896a92428bd11f65525763f
saxena-ishan <saxena.ishan2204@gmail.com>
2019-12-01 03:12:27 -0600

    added functions for setting max vars and years

--- src/redux/actions/actions.jsx
+++ src/redux/actions/actions.jsx
@@ -1,4 +1,31 @@
 import * as types from './actionTypes';
 
+export function updateYears(year, remove) {
+    return (dispatch, getState) => {
+        const { years } = getState();
+    
+        if (remove) {
+            dispatch({ 
+                type: types.UPDATE_YEARS,
+                years: years.filter((y) => y !== year)
+            });
+        }
+        else {
+            dispatch({ 
+                type: types.UPDATE_YEARS,
+                years: years.map((y) => y).push(year)
+            });
+        }
+    };
+}
+
+export const setSingleMax = (max) => {
+    return { type: types.SET_SINGLE_MAX, max };
+} 
+
+export const setTotalMax = (max) => {
+    return { type: types.SET_TOTAL_MAX, max };
+}
+
 
 
