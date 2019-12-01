9980d6133e7d1185c7e1132eea4097e82fe514d2
saxena-ishan <saxena.ishan2204@gmail.com>
2019-11-30 23:00:22 -0600

    implmented ring chart with basic features and dummy data

---
+++ src/components/RingChart/Ring.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { updateRing } from "../../utils/updateRing";
+
+export default class Ring extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            barTextHidden: true
+        }
+    }
+    componentDidUpdate() {
+    }
+
+    componentDidMount() {
+
+        const start = () => { this.setState({barTextHidden: true}); }
+        const end = () => { this.setState({barTextHidden: false}); }
+
+        updateRing(this.props.continent, 15, 100, start, end);
+    }
+    
+    
+    render() {
+
+        const { radius = 100, strokeWidth = 15, color = "red", x = 10, y = 20, continent = "L" } = this.props;
+        const { barTextHidden } = this.state;
+        return (
+            <g className="ring-root" transform={`translate(${x},${y})`}>
+
+                <circle
+                 cx={radius} 
+                 cy={radius} 
+                 r={radius}
+                 stroke={color}
+                 strokeWidth={strokeWidth}
+                 title={continent}
+                ><title>{continent}</title></circle>
+
+                <g className="ring-axis-elements">
+                    <line 
+                     x1={strokeWidth}
+                     y1={radius}
+                     x2={(radius * 2) - strokeWidth}
+                     y2={radius}
+                    />
+                    
+                    <text 
+                     x={strokeWidth}
+                     y={radius - 2} 
+                     textAnchor="start"
+                    >M</text>
+                    
+                    <text 
+                     x={(radius * 2) - strokeWidth} 
+                     y={radius + 12}
+                     textAnchor="end"
+                    >F</text>
+                </g>
+                
+                <g className="ring-bars">
+                    {/* Male Results */}
+                    <path className={`gold-bar ${continent} male`}>
+                        <title></title>
+                    </path>
+                    <text 
+                     className={`gold-bar-text ${continent} male`} 
+                     id={`bar-text-${(barTextHidden? "hidden" : "visible")}`}
+                     textAnchor="middle"
+                    />
+                    <path className={`silver-bar ${continent} male`}>
+                        <title></title>
+                    </path>
+                    <text className={`silver-bar-text ${continent} male`} id={`bar-text-${(barTextHidden? "hidden" : "visible")}`} textAnchor="middle"/>
+                    <path className={`bronze-bar ${continent} male`}>
+                        <title></title>
+                    </path>
+                    <text className={`bronze-bar-text ${continent} male`} id={`bar-text-${(barTextHidden? "hidden" : "visible")}`} textAnchor="middle"/>
+
+                    {/* Female Results */}
+                    <path className={`gold-bar ${continent} female`}>
+                        <title></title>
+                    </path>
+                    <text className={`gold-bar-text ${continent} female`} id={`bar-text-${(barTextHidden? "hidden" : "visible")}`} textAnchor="middle"/>
+                    <path className={`silver-bar ${continent} female`}>
+                        <title></title>
+                    </path>
+                    <text className={`silver-bar-text ${continent} female`} id={`bar-text-${(barTextHidden? "hidden" : "visible")}`} textAnchor="middle"/>
+                    <path className={`bronze-bar ${continent} female`}>
+                        <title></title>
+                    </path>
+                    <text className={`bronze-bar-text ${continent} female`} id={`bar-text-${(barTextHidden? "hidden" : "visible")}`} textAnchor="middle"/>
+                </g>
+            </g>
+        )
+    }
+}
+
+
+
+
---
+++ src/components/RingChart/index.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import Ring from "./Ring"
+
+export class RingChart extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+
+        const { radius = 100, innerGap = 50, width = 800, height = 500} = this.props;
+
+        var colors = ["blue", "black", "red", "yellow", "green"]
+
+        var initialXUpper =  (width - ((3 * (radius * 2)) + (innerGap * 2))) / 2;
+        var initialXLower =  (width - ((2 * (radius * 2)) + (innerGap * 1))) / 2; 
+
+        var initialY =  (height - (2 * (radius * 2))) / 2;
+        return (
+            <svg className="ring-container" width={width} height={height}>
+                {
+                    colors.map((c, i) => {
+                        if (i < 3) {
+                            let x = initialXUpper + (i * ((radius * 2) + innerGap));
+                            return (
+                                <Ring
+                                 radius={radius}
+                                 color={c}
+                                 x={x}
+                                 y={initialY}
+                                 key={i}
+                                 continent={c}
+                                />
+                            )
+                        }
+                        else {
+                            let x = initialXLower + ((i - 3) * ((radius * 2) + innerGap));
+                            return (
+                                <Ring
+                                 radius={radius}
+                                 color={c}
+                                 x={x}
+                                 y={initialY + (radius * 2)}
+                                 key={i}
+                                 continent={c}
+                                />
+                            )
+                        }
+                    })
+                }
+                
+            </svg>
+        )
+    }
+}
