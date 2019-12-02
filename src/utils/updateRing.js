fb95ce7ba5c6f5d9043b3939e0af02697098236e
saxena-ishan <saxena.ishan2204@gmail.com>
2019-12-01 20:37:57 -0600

    updated function to update data

--- src/utils/updateRing.js
+++ src/utils/updateRing.js
@@ -1,55 +1,53 @@
 import { select, easeCubic } from "d3";
 
-export function updateRing(continent, year, strokeWidth, radius, start, end) {
+export function updateRing(continent, year, data, max, strokeWidth, radius, start, end) {
 
     var innerGap = 5;
     var barWidth = 20;
 
     var initialPoint = strokeWidth + ((((radius * 2) - (strokeWidth * 2)) - ((barWidth * 3) + (innerGap * 2))) / 2); 
 
-    var fill = ["gold", "silver", "bronze"]
+    var fill = [["gold", "G"], ["silver", "S"], ["bronze", "B"]]
 
     fill.forEach((medal, i) => {
-
-
         let rotationPoint = initialPoint + ((barWidth + innerGap) * i) + (barWidth / 2) 
         let x = initialPoint + ((barWidth + innerGap) * i);
         let y = radius - 5
-        let height = 5 + Math.round(Math.random() * ((radius - 35) - 5))
+        let height = 5 + Math.round((data["males"][medal[1]] / max) * ((radius - 35) - 5))
         let width = barWidth
 
-        select(`path.${medal}-bar.${continent}-${year}.male`)
-            .attr('d', rect(x, y, barWidth / 2, 0, barWidth / 4))
+        select(`path.${medal[0]}-bar.${continent}-${year}.male`)
             .attr("transform", `rotate(180, ${rotationPoint}, ${radius - 3.5})`)
+            .attr('d', rect(x, y, barWidth / 2, 0, barWidth / 4))
             .transition()
             .ease(easeCubic)
             .duration(2000)
-            .attr('d', rect(x, y, barWidth / 2, height, barWidth / 4))
-            .on("start", start)
-            .on("end", end);
+            // .on("start", start)
+            // .on("end", end)
+            .attr('d', rect(x, y, barWidth / 2, height, barWidth / 4));
 
-        select(`text.${medal}-bar-text.${continent}-${year}.male`)
+        select(`text.${medal[0]}-bar-text.${continent}-${year}.male`)
             .attr('x', x + (width / 2))
             .attr('y', y - height - 6)
-            .text(`${height}`);
+            .text(`${data["males"][medal[1]]}`);
 
-        height = 5 + Math.round(Math.random() * ((radius - 35) - 5))
+        height = 5 + Math.round((data["females"][medal[1]] / max) * ((radius - 35) - 5))
         x = initialPoint + ((barWidth + innerGap) * i);
         y = radius + 1.5;
 
-        select(`path.${medal}-bar.${continent}-${year}.female`)
+        select(`path.${medal[0]}-bar.${continent}-${year}.female`)
             .attr('d', rect(x, y, barWidth / 2, 0, barWidth / 4))
             .transition()
             .ease(easeCubic)
             .duration(2000)
-            .attr('d', rect(x, y, barWidth / 2, height, barWidth / 4))
-            .on("start", start)
-            .on("end", end);
+            // .on("start", start)
+            // .on("end", end)
+            .attr('d', rect(x, y, barWidth / 2, height, barWidth / 4));
 
-        select(`text.${medal}-bar-text.${continent}-${year}.female`)
+        select(`text.${medal[0]}-bar-text.${continent}-${year}.female`)
             .attr('x', x + width / 2)
             .attr('y', y + height + 18)
-            .text(`${height}`);
+            .text(`${data["females"][medal[1]]}`);
     });
 }
 
